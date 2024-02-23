a471748118c6a4d6da7e76fabdb92ec2ed7dccdd
AissaBerenice <157132661+AissaBerenice@users.noreply.github.com>
2024-02-23 07:33:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
+st.title("Visualización Datos del Titanic")
+
 import streamlit as st
 import pandas as pd
-
-st.title("Esto es un titulo")
-st.header("Esto es un header")
-st.markdown("*Markdown Italiks*")
 
 df = pd.read_csv("train.csv")
 st.dataframe(df) 
