4ac231235e8d18fd46aae506838ffc15d97bdda4
AissaBerenice <157132661+AissaBerenice@users.noreply.github.com>
2024-02-23 07:30:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,16 @@ st.markdown("*Markdown Italiks*")
 df = pd.read_csv("train.csv")
 st.dataframe(df) 
 
+clase1 = df[(df["Pclass"] == 1)]
+clase2 = df[(df["Pclass"] == 2)]
+clase3 = df[(df["Pclass"] == 3)]
+
+st.title("Representacion Grafica de Clases ")
+
+categories = ['Primera Clase', 'Segunda Clase', 'Tercera Clase']
+values = [len(clase1), len(clase2), len(clase3)]
+
+plt.pie(values, labels=categories, autopct='%1.1f%%', startangle= 90)
+plt.axis('equal') 
+plt.title('Pie Chart Example')
+plt.show()
